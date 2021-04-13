d4e6cdfd19adb196ef890960ef2fbc942262a9d7
renmaobo <renmaobo@outlook.com>
2021-04-13 16:40:28 +0800

    protject create submit
    project create save change submit

---
+++ src/Extensions.DependencyInjection/Extensions.DependencyInjection/IContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extensions.DependencyInjection
+{
+    /// <summary>
+    /// 容器(接口)
+    /// </summary>
+    public interface IContainer
+    {
+    }
+}
