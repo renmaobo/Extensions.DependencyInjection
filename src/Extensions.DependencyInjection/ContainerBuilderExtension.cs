4e7eca7cfb8a498c92d294ac0a3e6ef8aae0048b
renmaobo <renmaobo@outlook.com>
2021-04-19 18:02:45 +0800

    1、修复编译错误 2、对容器实现做单例模式

--- src/Extensions.DependencyInjection/ContainerBuilderExtension.cs
+++ src/Extensions.DependencyInjection/ContainerBuilderExtension.cs
@@ -12,14 +12,17 @@ namespace Extensions.DependencyInjection
         /// <summary>
         /// 添加瞬时
         /// </summary>
-        /// <typeparam name="TService"></typeparam>
-        /// <typeparam name="TImplementation"></typeparam>
-        /// <param name="builder"></param>
-        public static void AddTransient<TService, TImplementation>(this IBuilder<IContainer> builder,LifeTime lifeTime = LifeTime.Transient, string alias = null, object[] param = null) where TImplementation : TService
+        /// <typeparam name="TService">服务</typeparam>
+        /// <typeparam name="TImplementation">实现</typeparam>
+        /// <param name="builder">构建器</param>
+        /// <param name="lifeTime">生命周期。默认为瞬时</param>
+        /// <param name="alias">别名</param>
+        /// <param name="param">构造参数</param>
+        public static void AddTransient<TService, TImplementation>(this IBuilder<IContainer> builder, LifeTime lifeTime = LifeTime.Transient, string alias = null, object[] param = null) where TImplementation : TService
         {
-            builder.Build();
+            IContainer container = builder.Build();
 
-            builder.BuildObject.Register<TService, TImplementation>(lifeTime, alias, param);
+            container.Register<TService, TImplementation>(lifeTime, alias, param);
         }
     }
 }
--- src/Extensions.DependencyInjection/IocContainerBuilder.cs
+++ src/Extensions.DependencyInjection/IocContainerBuilder.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Extensions.DependencyInjection.Container;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,12 +12,27 @@ namespace Extensions.DependencyInjection
     public class IocContainerBuilder : IBuilder<IContainer>
     {
         /// <summary>
+        /// 容器
+        /// </summary>
+        private static IContainer _container { get; set; }
+
+        /// <summary>
+        /// 单例锁
+        /// </summary>
+        private static object singleLock = new object();
+
+        /// <summary>
         /// 构建容器
         /// </summary>
         /// <returns></returns>
         public IContainer Build()
         {
-            throw new NotImplementedException();
+            if (_container == null)
+                lock (singleLock)
+                    if (_container == null)
+                        _container = new IocContainer();
+
+            return _container;
         }
 
         /// <summary>
